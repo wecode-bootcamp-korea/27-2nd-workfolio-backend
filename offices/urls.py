e710600169c254210916638067a9984e08adbdab
Taejun Kang <eslerkang@gmail.com>
2021-12-14 23:07:44 +0900

    [CREATED] created building list view + filtering

--- offices/urls.py
+++ offices/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 
-from offices.views import BuildingView, special_list
+from offices.views import BuildingView, special_list, BuildingListView
 
 urlpatterns = [
+    path('', BuildingListView.as_view()),
     path('/<int:building_id>', BuildingView.as_view()),
     path('/specials', special_list),
 ]
