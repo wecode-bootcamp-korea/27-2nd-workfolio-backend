bc6bd45699dbfba56984b4e62352e61ea4ff8620
Taejun Kang <eslerkang@gmail.com>
2021-12-15 22:34:41 +0900

    [CREATED] created reservation get method / test.py

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.views        import View
+from django.http         import JsonResponse
 
-# Create your views here.
+from reservations.models import Reservation
+
+from core.utils          import authorization
+
+
+class ReservationView(View):
+    @authorization
+    def get(self, request):
+        user = request.user
+        print(user)
+        reservations = Reservation.objects.select_related(
+            'office', 'office__building'
+        ).filter(user=user)
+
+        result = [
+            {
+                'id'                 : reservation.id,
+                'reservation_number' : reservation.reservation_number,
+                'building_id'        : reservation.office.building.id,
+                'building_name'      : reservation.office.building.name,
+                'office_name'        : reservation.office.name,
+                'office_image'       : reservation.office.image,
+                'check_in_date'      : reservation.check_in_date,
+                'check_out_date'     : reservation.check_out_date,
+                'headcount'          : reservation.headcount,
+                'total_price'        : (
+                    (reservation.check_out_date-reservation.check_in_date).days+1
+                )*reservation.office.price,
+                'is_deleted'         : reservation.is_deleted,
+            }
+            for reservation in reservations
+        ]
+
+        return JsonResponse({'MESSAGE': 'SUCCESS', 'RESULT': result}, status=200)
--- workfolio/urls.py
+++ workfolio/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('buildings', include('offices.urls')),
+    path('reservations', include('reservations.urls')),
 ]
