d72ab65d1e0663b3db4780af6c0693d5065debfb
Taejun Kang <eslerkang@gmail.com>
2021-12-15 14:56:37 +0900

    [CREATED] created special_list method

--- offices/tests.py
+++ offices/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from offices.models import Special
+
+class SpecialListTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        Special.objects.create(name='asdf', description='asdf', icon='asdf')
+
+    def test_success_speical_list_get_method(self):
+        response = self.client.get('/buildings/specials')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.json(), {
+            'MESSAGE': 'SUCCESS',
+            'RESULT': [
+                'asdf'
+            ]
+        })
+
+    def tearDown(self):
+        Special.objects.all().delete()
