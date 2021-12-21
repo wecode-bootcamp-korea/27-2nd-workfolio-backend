dd82f4397934364d9ba102c7ef9934ebb96e5206
Taejun Kang <eslerkang@gmail.com>
2021-12-21 09:28:49 +0900

    [MODIFIED] MINOR UPDATE(Refactoring)

--- offices/views.py
+++ offices/views.py
@@ -107,33 +107,32 @@ class BuildingListView(View):
                 'district': 'district__in'
             }
 
-            limit        = int(data.get('limit', 10) or 10)
-            offset       = int(data.get('offset', 0) or 0)
-            check_in     = data.get('check-in', None)
-            check_out    = data.get('check-out', None)
-            max_price    = int(data.get('max-price', 50000) or 50000)
-            min_price    = int(data.get('min-price', 0) or 0)
-            capacity     = int(data.get('headcount', 10) or 10)
-            order_by     = data.get('order-by', None)
-            order_by     = orders[order_by] if order_by in orders else 'id'
+            limit      = int(data.get('limit', 10) or 10)
+            offset     = int(data.get('offset', 0) or 0)
+            max_price  = int(data.get('max-price', 50000) or 50000)
+            min_price  = int(data.get('min-price', 0) or 0)
+            capacity   = int(data.get('headcount', 10) or 10)
+            check_in   = data.get('check-in', None)
+            check_out  = data.get('check-out', None)
+            search     = data.get('search', None)
+            order_by   = data.get('order-by', None)
+            order_by   = orders[order_by] if order_by in orders else 'id'
 
-            filter_set   = {
+            filter_set = {
                 filters.get(key): value
-                for (key, value) in dict(request.GET).items()\
+                for (key, value) in data.lists()\
                 if filters.get(key) and value != ['']
             }
-
-            search = data.get('search', None)
 
             if search:
                 filter_set['name__contains'] = search
 
             offices = Office.objects.exclude(
                 Q(
-                    Q(reservation__check_out_date__gt=check_in) &
-                    Q(reservation__check_in_date__lte=check_in)
+                    Q(reservation__check_out_date__gt = check_in) &
+                    Q(reservation__check_in_date__lte = check_in)
                 ) |
-                Q(reservation__check_in_date__range=[
+                Q(reservation__check_in_date__range = [
                     check_in,
                     datetime.date.fromisoformat(check_out) - datetime.timedelta(days=1)
                 ])
@@ -143,13 +142,13 @@ class BuildingListView(View):
             office_ids = [office[0] for office in offices]
 
             buildings = Building.objects.filter(
-                office__id__in = office_ids
+                office__id__in    = office_ids
             ).distinct().annotate(
-                max_price      = Max(F('office__price')),
-                min_price      = Min(F('office__price')),
-                max_capacity   = Max(F('office__capacity_max')),
-                min_capacity   = Min(F('office__capacity')),
-                popularity     = Count(F('office__reservation')),
+                max_price         = Max(F('office__price')),
+                min_price         = Min(F('office__price')),
+                max_capacity      = Max(F('office__capacity_max')),
+                min_capacity      = Min(F('office__capacity')),
+                popularity        = Count(F('office__reservation')),
             ).prefetch_related('buildingimage_set').filter(
                 max_price__gte    = min_price,
                 min_price__lte    = max_price,
@@ -159,8 +158,8 @@ class BuildingListView(View):
             ).order_by(order_by)
 
             result = {
-                'count': buildings.count(),
-                'buildings': [
+                'count'     : buildings.count(),
+                'buildings' : [
                     {
                         'id'           : building.id,
                         'name'         : building.name,
